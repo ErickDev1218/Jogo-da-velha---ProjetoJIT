3cd6a0e79d838c12e83b55d2728f345d9549848b
ErickDev1218 <erickpdm879@gmail.com>
2024-01-23 16:59:41 -0300

    Atualizacoes finais do jogo da velha

--- game.js
+++ game.js
@@ -1,14 +1,126 @@
 let position = ['','','','','','','','','']
 let playerTime = 0;
 let symbol = ['o','x']
+let winCond = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
+let countp1 = 2;
+let countp2 = 2;
 
-function move(pos){
+function Winner(){
+    for(let i of winCond){
+      let test1 = i[0];
+      let test2 = i[1];
+      let test3 = i[2];
+      
+      if(position[test1] === position[test2] && position[test1] === position[test3] && position[test1] !== ''){
+        return true;
+      }
+    }
+
+    return false;
+}
+
+function move(pos,p1,p2,para1,para2){
     position[pos] = symbol[playerTime]
 
-    if(playerTime === 1){
-        playerTime = 0
-    }else{
-        playerTime = 1
-    }
+    let champion = Winner()
+
+    setTimeout(() => {
+        if(champion){
+            
+            if(playerTime === 0){
+                window.alert("Há um vencedor! Parabéns player " + p1.value);
+                countp1+=1;     
+                para1.innerText = `Player 1: ${p1.value} ⭕. Fichas ${countp1}`
+                countp2-=1;
+                para2.innerText = `Player 2: ${p2.value} ❌. Fichas ${countp2}`
+            }else if(playerTime === 1){
+                window.alert("Há um vencedor! Parabéns player " + p2.value);
+                countp2+=1;
+                para2.innerText = `Player 2: ${p2.value} ❌. Fichas ${countp2}`
+                countp1-=1;
+                para1.innerText = `Player 1: ${p1.value} ⭕. Fichas ${countp1}`   
+            }
+            
+            let squares = document.querySelectorAll(".square")
+            squares.forEach((e)=>{
+                e.innerHTML = `<div class = ""></div>` 
+            })
+            position = ['','','','','','','','','']
+            playerTime = 0;
+            para1.style.backgroundColor = "#24AFC1";
+            para1.style.padding = "6px";
+            para1.style.borderRadius = "8px";
+
+            para2.style.backgroundColor = "";
+            para2.style.padding = "";
+            para2.style.borderRadius = "";
+            
+        }else if(!champion){
+            playerTime = playerTime === 0 ? 1 : 0
+        }
+        
+        let aux = 0;
+        for(let i of position){
+            if(i !== ''){
+                aux++;
+            }
+        }
+        if(aux === position.length){
+            //deu velha
+            window.alert("Velha! Empate, reinicie a partida!")
+        }
+        let squares = document.querySelectorAll(".square")
+        if(countp1 === -1){
+            //P1 não pode mais jogar.
+            for(let i of squares){
+                i.style.display = "none"
+            }
+            window.alert(`O player ${p1.value} não possui mais fichas!`)
+            countp1 = 0;
+            countp2 +=1;
+            para1.innerText = `Player 1: ${p1.value} ⭕. Fichas ${countp1}`
+            para2.innerText = `Player 2: ${p2.value} ❌. Fichas ${countp2}`
+            document.getElementById("rei").style.display = "none"
+
+            para2.style.backgroundColor = "#24AFC1";
+            para2.style.padding = "6px";
+            para2.style.borderRadius = "8px";
+
+            para1.style.backgroundColor = "";
+            para1.style.padding = "";
+            para1.style.borderRadius = "";
+
+
+        }
+        if(countp2 === -1){
+            for(let i of squares){
+                i.style.display = "none"
+            }
+            window.alert(`O player ${p2.value} não possui mais fichas!`)
+            countp2 = 0;
+            countp1 +=1;
+            para1.innerText = `Player 1: ${p1.value} ⭕. Fichas ${countp1}`
+            para2.innerText = `Player 2: ${p2.value} ❌. Fichas ${countp2}`
+            document.getElementById("rei").style.display = "none"
+
+            para1.style.backgroundColor = "#24AFC1";
+            para1.style.padding = "6px";
+            para1.style.borderRadius = "8px";
+
+            para2.style.backgroundColor = "";
+            para2.style.padding = "";
+            para2.style.borderRadius = "";
+        }
+
+    }, 50);
 }
--- interface.js
+++ interface.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let squares = document.querySelectorAll(".square")
 
     squares.forEach((square) => {
+        let p1 = document.getElementById("p1")
+        let p2 = document.getElementById("p2")
+        let para1 = document.getElementById("p1name")
+        let para2 = document.getElementById("p2name")
         square.addEventListener("click", (e) => {
-            move(e.target.id)
-            updateSquare();
+            move(e.target.id,p1,p2,para1,para2)
+            updateSquare();  
         })
     })
 })
@@ -28,10 +32,17 @@ document.getElementById("rei").addEventListener("click",()=>{
     })
     position = ['','','','','','','','','']
     playerTime = 0;
+    let para1 = document.getElementById("p1name")
+    let para2 = document.getElementById("p2name")
+    para1.style.backgroundColor = "#24AFC1";
+    para1.style.padding = "6px";
+    para1.style.borderRadius = "8px";
+
+    para2.style.backgroundColor = "";
+    para2.style.padding = "";
+    para2.style.borderRadius = "";
 })
 
-let countp1 = 10;
-let countp2 = 10;
 
 document.getElementById("botao").addEventListener("click",()=>{
     let p1 = document.getElementById("p1")
@@ -58,6 +69,10 @@ document.getElementById("botao").addEventListener("click",()=>{
                 if(sqr[sqr.length-1].style.display === "inline-block"){
                     document.getElementById("rei").style.display = "inline-block"
                 }
+
+                para1.style.backgroundColor = "#24AFC1";
+                para1.style.padding = "6px";
+                para1.style.borderRadius = "8px";
             },i*400)
 
             
@@ -67,4 +82,30 @@ document.getElementById("botao").addEventListener("click",()=>{
     }else{
         window.alert("ERROR: Verifique os nomes dos jogadores.")
     }
+})
+
+let divs = document.getElementsByClassName("tela");
+
+Array.from(divs).forEach((e) => {
+    e.addEventListener("click",() => {
+        let para1 = document.getElementById("p1name")
+        let para2 = document.getElementById("p2name")
+        if(playerTime === 1){
+            para1.style.backgroundColor = "#24AFC1";
+            para1.style.padding = "6px";
+            para1.style.borderRadius = "8px";
+
+            para2.style.backgroundColor = "";
+            para2.style.padding = "";
+            para2.style.borderRadius = "";
+        }else if(playerTime === 0){
+            para2.style.backgroundColor = "#24AFC1";
+            para2.style.padding = "6px";
+            para2.style.borderRadius = "8px";
+
+            para1.style.backgroundColor = "";
+            para1.style.padding = "";
+            para1.style.borderRadius = "";
+        }
+    })
 })
